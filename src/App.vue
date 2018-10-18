875ad7dbafaebac1529ec4746cd824a8435fc2a1
luodaxia <450910277@qq.com>
2018-10-18 11:18:33 +0800

    created branch dev and add function about mint-ui to use mt-button

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,46 @@
   <div id="app">
     <img src="./assets/logo.png">
     <HelloWorld/>
+    <button class="mint-button mint-button--primary btn" size="normal" @click="show">
+      <label class="mint-button-text">点击弹出Toast</label>
+    </button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import HelloWorld from "./components/HelloWorld";
+
+import { Toast } from "mint-ui";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld
+  },
+  methods: {
+    show() {
+      Toast({
+        message: "提示",
+        position: "buttom",
+        duration: 3000
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.btn {
+  margin-left: auto;
+  margin-right: auto;
+
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+
+import Vue from 'vue';
+import App from './App';
+//引入完整的mint-ui，需要注意的是，样式文件需要单独引入。
+//mint-ui 只在vue中使用，专为vue定制
+import MintUi from 'mint-ui';
+import 'mint-ui/lib/style.css';
+
+Vue.use(MintUi);
+
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
-  components: { App },
-  template: '<App/>'
+  components: {
+    App
+  },
+  template: '<App/>',
+  
 })
